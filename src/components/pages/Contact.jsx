ee227ecd969c60576884fed59a24020654479713
Brochb <brochbaltzer@gmail.com>
2023-11-19 12:39:01 -0700

    added profile picture, added imports +repush, for failure

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
         <div>
-            <h2>Contact me using any of the methods belwo</h2>
+            <h2>Contact me using any of the methods below</h2>
             <p>Github: https://github.com/brochb</p>
             <p>Email: brochbaltzer@gmail.com</p>
         </div>
