ebf592e918e8f1ba276e9f944a7216252ea10545
Brochb <brochbaltzer@gmail.com>
2023-11-21 22:24:49 -0700

    added downloadable resume

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -3,18 +3,11 @@ export default function Resume() {
         <div>
             <h1>Resume</h1>
             <p>
-                Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-                molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-                magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-                efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-                mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-                posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-                faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-                ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-                dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-                conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-                rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+                Download my resume by clicking the link below!
             </p>
+            <div><a href="../../../public/Resume-Brochb.pdf" download="Resume-Brochb.pdf">
+                Click here
+            </a></div>
         </div>
     );
 }
