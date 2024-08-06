5ca8248a154e155205daf9431e32002c885e12ff
Brochb <brochbaltzer@gmail.com>
2024-08-06 11:51:28 -0600

    changed CR date in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-            <p>&copy; 2023 Broch Baltzer. All rights reserved.</p>
+            <p>&copy; 2024 Broch Baltzer. All rights reserved.</p>
             <div className="social-links">
                 <section><a href="https://github.com/brochb" target="_blank" rel="noopener noreferrer">GitHub</a></section>
                 <section><a href="https://www.linkedin.com/in/broch-baltzer-90a337289/" target="_blank" rel="noopener noreferrer">LinkedIn</a></section>
