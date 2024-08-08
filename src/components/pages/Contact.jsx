82254f5fccdab66325e61210c379074bd1a98269
Brochb <brochbaltzer@gmail.com>
2024-08-08 10:11:23 -0600

    added link in contacts page that lets visitors send me an e-mail

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -4,6 +4,7 @@ export default function Contact() {
             <h2>Contact me using any of the methods below</h2>
             <p>Github: https://github.com/brochb</p>
             <p>LinkedIn: https://www.linkedin.com/in/brochb/</p>
+            <p>Click <a href="mailto:brochbaltzer@gmail.com">here</a> to send me an e-mail with information on how to apply to your job, or with a request for service.</p>
         </div>
     );
 }
